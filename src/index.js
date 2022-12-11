49c4e257a3032abdc20f8794b5b3b8c4c9e081be
jalmo1 <joshuarosamktp4@gmail.com>
2022-12-11 10:55:26 -0600

    Add Card component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App';
-import Hello from './Hello';
-import reportWebVitals from './reportWebVitals';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import Card from "./Card";
+import reportWebVitals from "./reportWebVitals";
+import "tachyons";
+import { robots } from "./robots";
+import CardList from "./CardList";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Hello greeting={'Hello ' + 'React Ninja'} />
+    <CardList robots={robots} />
   </React.StrictMode>
 );
 
